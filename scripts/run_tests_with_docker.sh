d03f4c5f679b7835e719a6b816bfc9788a58063a
Ahmad Alobaid <ahmad88me@gmail.com>
2018-11-05 17:29:11 +0100

    fixing codecov

--- scripts/run_tests_with_docker.sh
+++ scripts/run_tests_with_docker.sh
@@ -1,2 +1,3 @@
 docker image build -t fcm:latest .
-docker container run -e CODECOV_TOKEN=$CODECOV_TOKEN  --interactive --tty --rm --name fcm fcm:latest
+#docker container run -e CODECOV_TOKEN=$CODECOV_TOKEN  --interactive --tty --rm --name fcm fcm:latest
+docker container run  --interactive --tty --rm --name fcm fcm:latest
