071a77c8aefd866c91ea6030dde686a7a9f2560c
Ahmad Alobaid <ahmad88me@gmail.com>
2018-11-05 17:41:17 +0100

    update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 apt update
-apt install curl wget zip g++ cmake lcov -y
+apt install git curl wget zip g++ cmake lcov -y
 wget https://github.com/google/googletest/archive/release-1.8.1.zip
 unzip release-1.8.1.zip
 rm release-1.8.1.zip
