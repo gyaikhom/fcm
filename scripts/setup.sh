b828682b050bbed481474641d8a53976dcd1fc8c
Ahmad Alobaid <ahmad88me@gmail.com>
2018-11-21 19:13:58 +0100

    install Eigen c++ library

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,7 @@ wget https://github.com/google/googletest/archive/release-1.8.1.zip
 unzip release-1.8.1.zip
 rm release-1.8.1.zip
 cd googletest-release-1.8.1; cmake -G "Unix Makefiles"; make; make install; cd ..; rm -Rf googletest-release-1.8.1
+wget http://bitbucket.org/eigen/eigen/get/3.3.5.zip
+unzip 3.3.5.zip
+rm 3.3.5.zip
+cmake eigen-eigen-*; make install;rm -Rf *
